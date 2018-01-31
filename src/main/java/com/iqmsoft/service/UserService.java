a98c81d39ba2263799d491a5063e66c065f438cf
Davanon <davanon2014@gmail.com>
2018-01-31 10:17:52 +0530

    First

---
+++ src/main/java/com/iqmsoft/service/UserService.java
@@ -0,0 +1,22 @@
+package com.iqmsoft.service;
+
+import java.util.List;
+
+import com.iqmsoft.domain.User;
+
+public interface UserService {
+
+    boolean isUserExist(User user);
+
+    User saveUser(User user);
+
+    User findUserById(long id);
+
+    List<User> findAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long id);
+
+    void deleteAllUsers();
+}
